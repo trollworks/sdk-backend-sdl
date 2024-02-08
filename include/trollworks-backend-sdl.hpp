557a30250c2d59236bc336b9fb14ca16b11de323
David Delassus <david.jose.delassus@gmail.com>
2024-02-08 04:24:47 +0100

    :sparkles: include input system in root header

--- include/trollworks-backend-sdl.hpp
+++ include/trollworks-backend-sdl.hpp
@@ -4,3 +4,11 @@
 #include "./trollworks-backend-sdl/backend.hpp"
 #include "./trollworks-backend-sdl/assets.hpp"
 #include "./trollworks-backend-sdl/components.hpp"
+
+#include "./trollworks-backend-sdl/input/ui.hpp"
+#include "./trollworks-backend-sdl/input/state.hpp"
+#include "./trollworks-backend-sdl/input/actions.hpp"
+#include "./trollworks-backend-sdl/input/bindings.hpp"
+#include "./trollworks-backend-sdl/input/passthrough.hpp"
+#include "./trollworks-backend-sdl/input/controls.hpp"
+#include "./trollworks-backend-sdl/input/manager.hpp"
