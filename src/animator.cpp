391b23c95c7a02a12ac18ff222d4ffa02202b8cb
David Delassus <david.jose.delassus@gmail.com>
2024-05-27 18:07:59 +0200

    :construction: :recycle: update animator job to use new animation component

--- src/animator.cpp
+++ src/animator.cpp
@@ -5,23 +5,19 @@ namespace tw::sdl {
   void animator::update(float delta_time, void*) {
     auto& registry = scene_manager::main().registry();
 
-    auto animations = registry.view<sprite, animation>();
+    auto animations = registry.view<animation>();
 
     for (auto e_animation : animations) {
-      auto& c_sprite = animations.get<sprite>(e_animation);
       auto& c_animation = animations.get<animation>(e_animation);
+      auto& anim        = *c_animation.animation;
 
-      auto& current_frame = c_animation.frames[c_animation.frame_index];
-      c_animation.frame_time += delta_time;
+      auto& current_frame = anim.frames[anim.frame_index];
+      anim.frame_time += delta_time;
 
-      if (c_animation.frame_time >= current_frame.frame_duration) {
-        c_animation.frame_time -= current_frame.frame_duration;
-        c_animation.frame_index = (c_animation.frame_index + 1) % c_animation.frames.size();
+      if (anim.frame_time >= current_frame.duration) {
+        anim.frame_time -= current_frame.duration;
+        anim.frame_index = (anim.frame_index + 1) % anim.frames.size();
       }
-
-      auto& next_frame = c_animation.frames[c_animation.frame_index];
-      c_sprite.texture = next_frame.texture;
-      c_sprite.source = next_frame.source;
     }
   }
 }
