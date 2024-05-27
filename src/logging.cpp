32f8b390df7bc123cb756ee0281dabef5a98165a
David Delassus <david.jose.delassus@gmail.com>
2024-05-27 23:29:58 +0200

    :bug: add missing logger_printer implementation

--- src/logging.cpp
+++ src/logging.cpp
@@ -1,6 +1,10 @@
 #include "../include/trollworks-backend-sdl/logging.hpp"
 
 namespace tw::sdl::logging {
+  void logger_printer::operator()(const std::string& message) {
+    SDL_Log("%s", message.c_str());
+  }
+
   logger_type& logger() {
     if (!entt::locator<logger_type>::has_value()) {
       entt::locator<logger_type>::emplace(
