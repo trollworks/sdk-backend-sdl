eaaa2c87a0e1c61739f873c49b5a8983e5150075
David Delassus <david.jose.delassus@gmail.com>
2024-05-27 23:31:07 +0200

    :recycle: move logger printer implementation in details namespace

--- include/trollworks-backend-sdl/logging.hpp
+++ include/trollworks-backend-sdl/logging.hpp
@@ -7,11 +7,13 @@
 #include "./SDL.hpp"
 
 namespace tw::sdl::logging {
-  struct logger_printer {
-    void operator()(const std::string& message);
-  };
+  namespace details {
+    struct printer {
+      void operator()(const std::string& message);
+    };
+  }
 
-  using logger_type = logfmtxx::logger<std::chrono::system_clock, logger_printer>;
+  using logger_type = logfmtxx::logger<std::chrono::system_clock, details::printer>;
 
   logger_type& logger();
 }
